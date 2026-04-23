9f61a6fc59f28e671a00ca8f48f1339900e42d46
youssefamr24 <youssef.amrfcai@gmail.com>
2026-04-23 11:38:30 +0200

    Task 1: Implementing the factors from scratch is Done

---
+++ src/main.py
@@ -0,0 +1,25 @@
+# Task 1: Implement the Factors 
+
+# A-B
+def phi1(a, b):
+
+    if a == b:
+        return 4
+    else:
+        return 2
+
+# B-C
+def phi2(b, c):
+
+    if b == c:
+        return 5
+    else:
+        return 1
+
+# C-A
+def phi3(c, a):
+
+    if c == a:
+        return 3
+    else:
+        return 1
